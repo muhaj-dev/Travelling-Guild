7d3bdcea8eb186c0aa3aa3aaf8bbbbcb1f177409
muhammod1 <muhammodholawale@gmail.com>
2022-04-18 10:57:32 +0100

    update

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,7 @@ export const getPlacesData = async (type, sw, ne) => {
             },
             headers: {
               'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-              'x-rapidapi-key': '1b7725303bmsh00e39e1fe2166d6p1084e9jsn476336c0f82a'
+              'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
             }
           });
 
@@ -29,7 +29,7 @@ export const getWeatherData = async ( lat, lng) => {
       params: { lon: lng, lat: lat },
       headers: {
         'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-        'X-RapidAPI-Key': '1b7725303bmsh00e39e1fe2166d6p1084e9jsn476336c0f82a'
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
       }
     })
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -13,7 +13,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCu3mjHmVk5nFs1QX1TUw-3zVOjXf1G9wg'}}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
